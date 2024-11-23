728a842351b50b8f4b4296058cce11bf9347b828
Atakan Demircioğlu <31544037+atakde@users.noreply.github.com>
2024-11-23 10:12:06 +0300

    chore(leetcode): remove debugging code block

--- rotaating-the-box.php
+++ rotaating-the-box.php
@@ -31,13 +31,5 @@ class Solution
                 }
             }
         }
-
-        for ($i = 0; $i < $cols; $i++) {
-            for ($j = 0; $j < $rows; $j++) {
-                echo $newArr[$i][$j];
-            }
-
-            echo PHP_EOL;
-        }
     }
 }
